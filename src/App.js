73755c2fc37a08c7a8729aa2f21acf9f34c8b001
Arnav Singh <36072753+itsarnavsingh@users.noreply.github.com>
2023-02-10 01:18:43 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { movies } from "./movies";
+
+const Container = styled.div`
+  width: 100vw;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+  gap: 100px;
+  height: 100vh;
+  background-color: #111;
+  color: #fff;
+  padding: 50px 0;
+  overflow-y: scroll;
+`;
+const Top = styled.div``;
+
+const SearchBar = styled.input`
+  border-radius: 5px;
+  height: 50px;
+  width: 62vw;
+  padding: 0 25px;
+  font-size: 25px;
+  text-align: center;
+  color: #fff;
+  outline: none;
+  border: none;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const Movies = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 50px;
+  width: 62vw;
+`;
+
+const MovieContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  width: 250px;
+`;
+
+const MovieImage = styled.img`
+  width: 250px;
+  height: 330px;
+  border-radius: 5px;
+  overflow: hidden;
+  object-fit: cover;
+`;
+
+const MovieTitle = styled.h1`
+  margin: 0;
+  padding: 0;
+`;
+
+const MovieDescription = styled.div`
+  opacity: 0.5;
+`;
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  const filteredMovies = movies.filter((movie) => {
+    return movie.name.toLocaleLowerCase().includes(query);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Top>
+        <SearchBar
+          placeholder="Search Movies"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+      </Top>
+
+      <Movies>
+        {filteredMovies.map((item) => (
+          <Movie kye={item.id} {...item} />
+        ))}
+      </Movies>
+    </Container>
+  );
+}
+
+function Movie({ img, name, description }) {
+  return (
+    <MovieContainer>
+      <MovieImage src={img} />
+      <MovieTitle>{name}</MovieTitle>
+      <MovieDescription>{description}</MovieDescription>
+    </MovieContainer>
   );
 }
 
---
+++ src/movies.js
@@ -0,0 +1,72 @@
+export const movies = [
+  {
+    id: 1,
+    name: "Fatal Affair",
+    img: "https://img.netnaija.com/Q6bHHK.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ab, harum laudantium nisi repellendus modi quia! Sunt odio alias quas.",
+  },
+  {
+    id: 2,
+    name: "Gundala",
+    img: "https://img.netnaija.com/yFbHHK.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ab, harum laudantium nisi repellendus modi quia! Sunt odio alias quas.",
+  },
+  {
+    id: 3,
+    name: "Attraction",
+    img: "https://img.netnaija.com/CTbHHK.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ab, harum laudantium nisi repellendus modi quia! Sunt odio alias quas.",
+  },
+  {
+    id: 4,
+    name: "SamSam",
+    img: "https://img.netnaija.com/L6bHHK.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ab, harum laudantium nisi repellendus modi quia! Sunt odio alias quas.",
+  },
+  {
+    id: 5,
+    name: "The Sunlit Night",
+    img: "https://img.netnaija.com/R6bHHK.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ab, harum laudantium nisi repellendus modi quia! Sunt odio alias quas.",
+  },
+  {
+    id: 6,
+    name: "The Outpost",
+    img: "https://img.netnaija.com/_VbHHK.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ab, harum laudantium nisi repellendus modi quia! Sunt odio alias quas.",
+  },
+  {
+    id: 7,
+    name: "The Old Guard",
+    img: "https://img.netnaija.com/QmbHHK.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ab, harum laudantium nisi repellendus modi quia! Sunt odio alias quas.",
+  },
+  {
+    id: 8,
+    name: "Archive",
+    img: "https://img.netnaija.com/FmbHHK.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ab, harum laudantium nisi repellendus modi quia! Sunt odio alias quas.",
+  },
+  {
+    id: 9,
+    name: "First Cow",
+    img: "https://img.netnaija.com/TmbHHK.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ab, harum laudantium nisi repellendus modi quia! Sunt odio alias quas.",
+  },
+  {
+    id: 10,
+    name: "Money Plane",
+    img: "https://img.netnaija.com/LwbHHK.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ab, harum laudantium nisi repellendus modi quia! Sunt odio alias quas.",
+  },
+];
